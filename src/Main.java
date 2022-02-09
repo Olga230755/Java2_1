5f0ff98af14c3cdb0aa491dcd2905c4439ba7077
Olga.Fedotova <bursh1-ola@yandex.ru>
2022-02-09 19:50:04 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 150;
+        int bonus = 20;
+        int sumbonus = price / bonus;
+        System.out.println("Количество бонусных миль:" + sumbonus);
+
+
+    }
+
+}
